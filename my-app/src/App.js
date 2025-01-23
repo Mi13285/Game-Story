ad1305e357d8e793e03183e98ad7f6d9bf40387e
Mi13285 <mixkud95@inbox.ru>
2025-01-23 19:53:21 +0300

    game page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,20 +3,22 @@ import { Header } from "./components/header/header";
 import { HomePage } from "./pages/home-page";
 // import { GamePage } from "./pages/game-page";
 import { Provider } from "react-redux";
-// import { store } from "./store";
+import { store } from "./redux";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route exact path="/app/:title"></Route>
-          {/* <Route index element={<GamePage />} /> */}
-          <Route index element={<HomePage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route exact path="/app/:title"></Route>
+            {/* <Route index element={<GamePage />} /> */}
+            <Route index element={<HomePage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 export default App;
--- my-app/src/components/cart-block/cart-block.js
+++ my-app/src/components/cart-block/cart-block.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./cart-block.css";
 import { ShoppingCart } from "lucide-react";
 export const CartBlock = () => {
+  const items = useSelector((state) => state.cart.itemsInCart);
+  const totalPrice = items.reduce((acc, game) => (acc += game.price), 0);
   return (
     <div className="cart-block">
       <ShoppingCart size={25} className="cart-block__icon" />
-      <span className="cart-block__total-price">2313</span>
+      <span className="cart-block__total-price">{totalPrice}</span>
     </div>
   );
 };
