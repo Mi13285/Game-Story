c969bbc4c9a616ecb60e3fb8f89542415c7300e6
Mi13285 <mixkud95@inbox.ru>
2025-01-24 08:24:24 +0300

    The add product translator button

--- my-app/src/components/cart-block/cart-block.js
+++ my-app/src/components/cart-block/cart-block.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { CartMenu } from "../cart-menu";
 import "./cart-block.css";
 import { ShoppingCart } from "lucide-react";
+import { calcTotalPrice } from "../utils";
 export const CartBlock = () => {
   const items = useSelector((state) => state.cart.itemsInCart);
-  const totalPrice = items.reduce((acc, game) => (acc += game.price), 0);
+  const totalPrice = calcTotalPrice(items);
   return (
     <div className="cart-block">
       <ShoppingCart size={25} className="cart-block__icon" />
-      <span className="cart-block__total-price">{totalPrice}</span>
+      <span className="cart-block__total-price">{totalPrice}руб</span>
+      <CartMenu items={items} onClick={() => null} />
     </div>
   );
 };
