f0616e4ec933eb61c1640c0d1c772f02ffe09378
Mi13285 <mixkud95@inbox.ru>
2025-01-25 07:48:23 +0300

    The add product translator button

--- my-app/src/components/cart-block/cart-block.js
+++ my-app/src/components/cart-block/cart-block.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { CartMenu } from "../cart-menu";
 import "./cart-block.css";
 import { ShoppingCart } from "lucide-react";
 import { calcTotalPrice } from "../utils";
 export const CartBlock = () => {
+  const [isCartMenuVisible, setIsCartMenuVisible] = useState(false);
   const items = useSelector((state) => state.cart.itemsInCart);
   const totalPrice = calcTotalPrice(items);
   return (
     <div className="cart-block">
-      <ShoppingCart size={25} className="cart-block__icon" />
-      <span className="cart-block__total-price">{totalPrice}руб</span>
-      <CartMenu items={items} onClick={() => null} />
+      <ShoppingCart
+        size={25}
+        className="cart-block__icon"
+        onClick={() => setIsCartMenuVisible(!isCartMenuVisible)}
+      />
+      {totalPrice > 0 ? (
+        <span className="cart-block__total-price">{totalPrice}руб</span>
+      ) : null}
+      {isCartMenuVisible && <CartMenu items={items} onClick={() => null} />}
     </div>
   );
 };
--- my-app/src/components/game-buy/game-buy.js
+++ my-app/src/components/game-buy/game-buy.js
@@ -1,21 +1,31 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { setItemInCart } from "../../redux/cart/reducer";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteItemFromCart, setItemInCart } from "../../redux/cart/reducer";
 import { Button } from "../button";
 import "./game-buy.css";
 
 export const GameBuy = ({ game }) => {
   const dispatch = useDispatch();
+  const items = useSelector((state) => state.cart.itemsInCart);
+  const isItemInCart = items.some((item) => item.id === game.id);
+
   const handleClick = (e) => {
     e.stopPropagation();
-    dispatch(setItemInCart(game));
+    if (isItemInCart) {
+      dispatch(deleteItemFromCart(game.id));
+    } else {
+      dispatch(setItemInCart(game));
+    }
   };
 
   return (
     <div className="game-buy">
-      <span className="game-buy__price">{game.price}руб</span>
-      <Button type="primary" onClick={handleClick}>
-        В Корзину
+      <span className="game-buy__price">{game.price} руб.</span>
+      <Button
+        type={isItemInCart ? "secondary" : "primary"}
+        onClick={handleClick}
+      >
+        {isItemInCart ? "Убрать из корзины" : "В Корзину"}
       </Button>
     </div>
   );
